f1ab36a9af28fdf446ec9ec83215e1172cda58ce
mzazakeith <mzazakeith@gmail.com>
2021-11-04 14:43:57 +0300

    login and home controller added

--- src/main/java/com/example/springsecuritydemo/controller/UserRegistrationController.java
+++ src/main/java/com/example/springsecuritydemo/controller/UserRegistrationController.java
@@ -1,23 +1,33 @@
 package com.example.springsecuritydemo.controller;
 
+import com.example.springsecuritydemo.dto.UserRegistrationDto;
 import com.example.springsecuritydemo.model.User;
 import com.example.springsecuritydemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("registration")
+@Controller
+@RequestMapping("/registration")
 public class UserRegistrationController {
     @Autowired
     private UserService userService;
 
-    @PostMapping()
-    public ResponseEntity<User> saveUser(@RequestBody User user){
-        return new ResponseEntity<User>(userService.saveUser(user), HttpStatus.CREATED);
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto(){
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm(){
+        return "registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto){
+        userService.saveUser(registrationDto);
+        return "redirect:/registration?success";
     }
 }
