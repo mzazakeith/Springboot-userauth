c1149905b14880183da5d2d00c08cfb94cc74978
mzazakeith <mzazakeith@gmail.com>
2021-11-04 14:46:26 +0300

    changes in repository, entities and user service for spring security

--- src/main/java/com/example/springsecuritydemo/model/Role.java
+++ src/main/java/com/example/springsecuritydemo/model/Role.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-@Data
 @Entity
 @Table(name = "role")
 public class Role {
@@ -12,4 +11,27 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/springsecuritydemo/service/UserService.java
+++ src/main/java/com/example/springsecuritydemo/service/UserService.java
@@ -1,17 +1,46 @@
 package com.example.springsecuritydemo.service;
 
+import com.example.springsecuritydemo.dto.UserRegistrationDto;
+import com.example.springsecuritydemo.model.Role;
 import com.example.springsecuritydemo.model.User;
 import com.example.springsecuritydemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Autowired
     private UserRepository userRepository;
 
-    public User saveUser(User user) {
+    public User saveUser(UserRegistrationDto registrationDto) {
+        User user = new User(registrationDto.getFirstName(),registrationDto.getLastName(),registrationDto.getEmail(),passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList((new Role("ROLE_USER"))));
         return userRepository.save(user);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null){
+            throw new UsernameNotFoundException("Invalid email provided");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+    }
 }
